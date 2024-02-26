c74b401c1d8d3b860e97ef18153ed4bd0af6ce8c
Sharaf Qeshta <sharaf.keshta@gmail.com>
2024-02-25 21:28:51 -0500

    PP 3.4

---
+++ Chapter_03/Project_03_04/Project_03_04.java
@@ -0,0 +1,14 @@
+package chapter_01;
+
+/**
+ * PP 3.4 The array implementation in this chapter keeps the top variable
+ * pointing to the next array position above the actual top of the
+ * stack. Rewrite the array implementation such that stack[top] is
+ * the actual top of the stack.
+ *
+ * @author Sharaf Qeshta
+ * */
+public class Project_03_04
+{
+    // look at ArrayStack.java
+}
