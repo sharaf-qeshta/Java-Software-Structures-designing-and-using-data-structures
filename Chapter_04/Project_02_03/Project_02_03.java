3e4648ddc655dfba8e059f87972b49916e741bc7
Sharaf Qeshta <sharaf.keshta@gmail.com>
2024-03-14 21:24:43 -0400

    PP 4.3

---
+++ Chapter_04/Project_02_03/Project_02_03.java
@@ -0,0 +1,12 @@
+package chapter_04;
+
+/**
+ * PP 4.3 Create a new version of the LinkedStack<T> class that makes
+ * use of a dummy record at the head of the list.
+ *
+ * @author Sharaf Qeshta
+ * */
+public class Project_02_03
+{
+    // look at LinkedStackWithDummyHead.java
+}
