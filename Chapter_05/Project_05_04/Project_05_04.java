277195521d30076ba6c92f2294a851e50e1f46a9
Sharaf Qeshta <sharaf.keshta@gmail.com>
2024-05-19 03:05:01 -0400

    PP 5.4

---
+++ Chapter_05/Project_05_04/Project_05_04.java
@@ -0,0 +1,16 @@
+package chapter_05;
+
+/**
+ * PP 5.4 All of the implementations in this chapter use a count variable to
+ * keep track of the number of elements in the queue. Rewrite the
+ * linked implementation without a count variable.
+ *
+ * @author Sharaf Qeshta
+ * */
+public class Project_05_04
+{
+    public static void main(String[] args)
+    {
+        // see LinkedQueueWithoutCount.java
+    }
+}
